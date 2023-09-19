49abae91b2e34b4561136a3a979203b3f1257c2b
ruben <rjulian@elpuig.xeill.net>
2023-09-20 00:15:41 +0200

    Initial commit

---
+++ src/CreaDir.java
@@ -0,0 +1,23 @@
+import java.io.File;
+import java.util.Scanner;
+
+public class CreaDir {
+    public static void main(String[] args) {
+
+        Scanner input = new Scanner(System.in);
+
+        System.out.println("Elige la ruta del directorio incluyendo el directorio que quieres crear");
+
+        String dir = input.nextLine();
+
+        File f = new File(dir);
+        
+        if (f.exists()){
+            System.out.println("No ha sigut possible crear el directori");
+        }
+        else {
+            f.mkdirs();
+            System.out.println(f);
+        }
+    }
+}
