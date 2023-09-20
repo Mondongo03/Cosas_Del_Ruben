c6dedac205df2287e16632dbb00964527aedb7aa
ruben <rjulian@elpuig.xeill.net>
2023-09-20 23:10:05 +0200

    Hasta los huevos del desordend de archivos

--- src/FileReaderAlpha.java
+++ src/FileReaderAlpha.java
@@ -2,22 +2,21 @@ import java.io.BufferedReader;
 import java.io.FileReader;
 
 public class FileReaderAlpha {
-    public static void main(String[] args){
+ public static void main(String[] args) {
         boolean finals = false;
         String letra;
         try {
             FileReader file = new FileReader("./alphabeta5465.txt");
             BufferedReader reader = new BufferedReader(file);
 
-            while (!finals){
+            while (!finals) {
                 letra = reader.readLine();
                 if (letra == null) finals = true;
-                else System.out.print(letra.toUpperCase()+" ");
+                else System.out.print(letra.toUpperCase() + " ");
 
             }
-        }catch (Exception e){
+        } catch (Exception e) {
             System.out.println("Error");
         }
 
-    }
-}
+    }}
